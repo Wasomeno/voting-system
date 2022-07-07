4c38d815abd0bbc63c03fac90b369158500104d9
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-07-07 18:37:04 +0700

    Gas Optimizations

--- contracts/VoterDataMerkle.sol
+++ contracts/VoterDataMerkle.sol
@@ -15,10 +15,10 @@ contract VoterDataMerkle {
     mapping(uint => bytes32[]) public votingToLeaves;
     mapping(address => History[]) public voterHistory;
 
-    function addLeaf(uint _votingId, bytes32 _leaf, bytes32 _newRoot) public {
+    function addLeaf(uint _votingId, bytes32 _leaf, bytes32 _newRoot) external {
         bytes32 defaultValue = 0xcfee7c08a98f4b565d124c7e4e28acc52e1bc780e3887db0a02a7d2d5bc66728;
         bool result = checkLeaf(_votingId, _leaf);
-        require(result == true, "The same leaf already added to the tree");
+        require(result, "The same leaf already added to the tree");
         votingToLeaves[_votingId].push(_leaf);
         votingToLeaves[_votingId].push(defaultValue);
         setRoot(_votingId, _newRoot);
@@ -28,37 +28,38 @@ contract VoterDataMerkle {
         votingToRoot[_votingId] = _root;
     }
 
-    function checkLeaf(uint _votingId, bytes32 _leaf) public view returns(bool){
+    function checkLeaf(uint _votingId, bytes32 _leaf) internal view returns(bool result){
         bool result;
         bytes32[] memory leaves = votingToLeaves[_votingId];
-        if(leaves.length == 0) {
+        uint length = leaves.length;
+        if(length == 0) {
             result = true;
         } else {
-            for(uint i; i < leaves.length; i++ ) {
+            for(uint i; i < length; i++ ) {
+            bytes32 leave = leaves[i];
             if(leaves[i] != _leaf) {
                 result = true;
                 }
             }
         }
         
-        return result;
     }
 
-    function getLeaves(uint _votingId) public view returns(bytes32[] memory) {
+    function getLeaves(uint _votingId) external view returns(bytes32[] memory) {
         bytes32[] memory leaves = votingToLeaves[_votingId];
         return leaves;
     }
     
-    function verify(uint _votingId, bytes32[] calldata proof, bytes32 leaf) public view returns(bool){
+    function verify(uint _votingId, bytes32[] calldata proof, bytes32 leaf) external view returns(bool){
         bytes32 root = votingToRoot[_votingId];
         return MerkleProof.verify(proof, root, leaf);
     }
 
-    function addVoterHistory(address _voter, uint _votingId, uint _candidateId, uint _timeStamp) public{
+    function addVoterHistory(address _voter, uint _votingId, uint _candidateId, uint _timeStamp) external{
         voterHistory[_voter].push(History(_votingId, _candidateId, _timeStamp));
     }
 
-    function getVoterHistory(address _voter) public view returns(History[] memory) {
+    function getVoterHistory(address _voter) external view returns(History[] memory) {
         History[] memory history = voterHistory[_voter];
         return history;
     }
--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -35,7 +35,7 @@ contract Voting {
     mapping(uint => History[]) public votingHistory;
     mapping(uint => address[]) public voterVerified;
 
-    function setInterface(address _ballotToken, address _voterData) public {
+    function setInterface(address _ballotToken, address _voterData) external {
         ballotInterface = IERC20(_ballotToken);
         voterDataInterface = IVoterData(_voterData);
     }
@@ -46,12 +46,14 @@ contract Voting {
         details.startTime = block.timestamp;
     }
 
-    function createVoting(bytes32 _title, uint _duration, uint _startTime, uint _roomId, bytes32[] calldata _candidates) public{
+    function createVoting(bytes32 _title, uint _duration, uint _startTime, uint _roomId, bytes32[] calldata _candidates) external{
         Detail storage details = votingDetails[votingCount];
         details.title = _title;
         details.duration = _duration;
-        for(uint i; i < _candidates.length; ++i) {
-            details.candidates.push(Candidate(i + 1,_candidates[i], 0));
+        uint length = _candidates.length;
+        for(uint i; i < length; ++i) {
+            bytes32 candidate = _candidates[i];
+            details.candidates.push(Candidate(i + 1, candidate, 0));
         }
         details.startTime = (_startTime * 1 hours) + block.timestamp;
         details.roomId = _roomId;
@@ -59,15 +61,18 @@ contract Voting {
         votingCount ++;
     }
 
-    function vote(uint _votingId, address _voter, uint _candidate) public {
-        Detail storage details = votingDetails[_votingId];
+    function vote(uint _votingId, address _voter, uint _candidate) external {
+        Detail memory details = votingDetails[_votingId];
         bool verified = checkVerifiedVoter(_votingId, _voter);
         bool voted = checkVoterVote(_votingId, _voter);
-        uint duration = details.startTime + (details.duration * 1 hours);
+        uint startTime = details.startTime;
+        uint duration = details.duration;
+        uint totalDuration = startTime + (duration * 1 hours);
+
         require(verified == true, "You are not verified");
         require(voted == false, "You already voted to one of the candidates");
-        require(details.startTime < block.timestamp, "Voting session has not started");
-        require(duration > block.timestamp, "Duration of the voting session is over");
+        require(startTime < block.timestamp, "Voting session has not started");
+        require(totalDuration > block.timestamp, "Duration of the voting session is over");
         uint index = getCandidateIndex(_votingId, _candidate);
         ballotInterface.approve(_voter, 1);
         ballotInterface.transfer(address(this), 1);
@@ -79,60 +84,56 @@ contract Voting {
     }
 
 
-    function getCandidateIndex(uint _votingId, uint _candidate) internal view returns(uint) {
-        uint index;
+    function getCandidateIndex(uint _votingId, uint _candidate) internal view returns(uint index) {
         Candidate[] memory  candidates= votingDetails[_votingId].candidates;
-        for(uint i; i < candidates.length; ++i) {
-            if(candidates[i].candidateId == _candidate) {
+        uint length = candidates.length;
+        for(uint i; i < length; ++i) {
+            uint candidate = candidates[i].candidateId;
+            if(candidate == _candidate) {
                 index = i;
             }
-        }
-        
-        return index;   
+        }  
     }
 
-    function getCandidates(uint _votingId) public view returns(Candidate[] memory) {
-        Candidate[] memory  candidates= votingDetails[_votingId].candidates;
-        return candidates;
+    function getCandidates(uint _votingId) external view returns(Candidate[] memory candidates) {
+        candidates = votingDetails[_votingId].candidates;
     }
 
-    function getHistory(uint _votingId) public view returns(History[] memory) {
-        History[] memory history = votingHistory[_votingId];
-        return history;
+    function getHistory(uint _votingId) external view returns(History[] memory history) {
+        history = votingHistory[_votingId];
     }
 
-    function checkVerifiedVoter(uint _votingId, address _voter) public view returns(bool) {
-        bool result;
+    function checkVerifiedVoter(uint _votingId, address _voter) internal view returns(bool result) {
         address[] memory verified = voterVerified[_votingId];
-        for(uint i; i < verified.length; ++i) {
-            if(verified[i] ==_voter) {
+        uint length = verified.length;
+        for(uint i; i < length; ++i) {
+            address _verified = verified[i];
+            if(_verified ==_voter) {
                 result = true;
             } else {
                 result = false;
             }
         }
-
-        return result;
     }
     
-    function checkVoterVote(uint _votingId, address _voter) public view returns(bool) {
-        bool result;
+    function checkVoterVote(uint _votingId, address _voter) internal view returns(bool result) {
         History[] memory votes = votingHistory[_votingId];
-        for(uint i; i < votes.length; i++){
-            if(votes[i].voter == _voter){
+        uint length = votes.length;
+        for(uint i; i < length; i++){
+            address voter = votes[i].voter;
+            if(voter == _voter){
                 result = true;
             } else {
                 result = false;
             }
         }
-        return result;
     }
 
-    function verifyVoter(uint _votingId, bytes32[] calldata proof, bytes32 leaf) public {
+    function verifyVoter(uint _votingId, bytes32[] calldata proof, bytes32 leaf) external {
         bool verify = voterDataInterface.verify(_votingId, proof, leaf);
         bool verified = checkVerifiedVoter(_votingId, msg.sender);
-        require(verify == true, "You are not a verified voter");
-        require(verified == false, "You are already verified");
+        require(verify, "You are not a verified voter");
+        require(!verified, "You are already verified");
         voterVerified[_votingId].push(msg.sender);
         ballotInterface.approve(address(this), 1);
         ballotInterface.transferFrom(address (this), msg.sender, 1);
